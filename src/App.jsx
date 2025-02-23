34201e551eff93ca7a251ae1570062a4b7ed4694
aerth saraogi <aerthsaraogi@192.168.1.2>
2025-02-23 17:40:20 +0530

    Bug fixed

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Navbar from './section/Navbar'
 import Hero from './section/Hero'
 import About from './section/About'
-import Project from './section/project'
+import Project from './section/Project'
 import Contact from './section/Contact'
 import Footer from './section/Footer'
 
